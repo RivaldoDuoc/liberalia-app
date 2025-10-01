6ebbcc8b051c3dce49793ce1efb4146cfe96a094
Nicolás Correa <ncorreaa@falabella.cl>
2025-09-30 23:11:19 -0300

    deploy test 4

--- liberalia/settings.py
+++ liberalia/settings.py
@@ -107,8 +107,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-FORCE_SCRIPT_NAME = '/novedades'
-STATIC_URL = '/static/'
+STATIC_URL = '/novedades/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
